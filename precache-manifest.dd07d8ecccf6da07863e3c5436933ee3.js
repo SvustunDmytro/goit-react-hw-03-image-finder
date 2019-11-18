6619bbfe3fdac80397cce989bf76d7be086ab3e4
SvisT <svisteckij@gmail.com>
2019-11-18 14:13:30 +0200

    Updates

--- precache-manifest.dd07d8ecccf6da07863e3c5436933ee3.js
+++ precache-manifest.dd07d8ecccf6da07863e3c5436933ee3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "947d35f716d7c29d55600f09501d078d",
+    "revision": "99f1e6755f3bd975e6f7cfdc9306625f",
     "url": "/goit-react-hw-03-image-finder/index.html"
   },
   {
-    "revision": "e3e91b2d329f9e243de5",
+    "revision": "f5e314054d864c80cc7d",
     "url": "/goit-react-hw-03-image-finder/static/css/main.20ec5479.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-03-image-finder/static/js/2.dbc479c1.chunk.js"
   },
   {
-    "revision": "e3e91b2d329f9e243de5",
-    "url": "/goit-react-hw-03-image-finder/static/js/main.cce2616b.chunk.js"
+    "revision": "f5e314054d864c80cc7d",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.189c2fe2.chunk.js"
   },
   {
     "revision": "d5c6c84b5bbdb1dbb1ca",
